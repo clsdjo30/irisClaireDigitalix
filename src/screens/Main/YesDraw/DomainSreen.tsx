7b2a6be6eddaf29b6c6bc4dd8acf95180149b440
clsdjo30 <lesergent.cedric@gmail.com>
2023-05-30 16:58:18 +0200

    feat(main) modify domainScreen- add images

--- src/screens/Main/YesDraw/DomainSreen.tsx
+++ src/screens/Main/YesDraw/DomainSreen.tsx
@@ -15,6 +15,13 @@ import { colors } from '../../../theme';
 import { useQuestionStore } from '../../../utils/hooks/useQuestionStore';
 import { StackScreenProps } from '@react-navigation/stack';
 
+// import icons
+const love = require('../../../../assets/images/testVector/love.png');
+const money = require('../../../../assets/images/testVector/finance.png');
+const work = require('../../../../assets/images/testVector/career.png');
+const general = require('../../../../assets/images/testVector/question_plus.png');
+const rightArrow = require('../../../../assets/icons/caretRight.png');
+
 interface YesDrawScreenProps {
     navigation: any;
 }
@@ -28,6 +35,7 @@ const SCREEN_HEIGHT = SCREEN_WIDTH * 1.5;
 const SCREEN_SCALE = Dimensions.get('window').scale;
 const SCREEN_FONT_SCALE = SCREEN_SCALE * 0.5;
 
+
 const DomainScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
     const [value, setValue] = useQuestionStore();
 
@@ -36,26 +44,26 @@ const DomainScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
     }
 
     function loveChoice() {
-        setValue({...value, domain:'amour'});
+        setValue({ ...value, domain: 'amour' });
         goToAskQuestionCard();
     }
 
     function workChoice() {
-        setValue({...value, domain:'travail'});
+        setValue({ ...value, domain: 'travail' });
         goToAskQuestionCard();
     }
 
     function moneyChoice() {
-        setValue({...value, domain:'argent'});
+        setValue({ ...value, domain: 'argent' });
         goToAskQuestionCard();
     }
 
     function generalChoice() {
-        setValue({...value, domain:'general'});
+        setValue({ ...value, domain: 'general' });
         goToAskQuestionCard();
     }
 
-    
+
     return (
         <LinearGradient
             // Card Linear Gradient
@@ -66,20 +74,32 @@ const DomainScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
                     <Text style={styles.contentTitle}>Choisissez votre domaine</Text>
                 </View>
                 <Pressable style={styles.domainCard} onPress={loveChoice}>
-                    <Icon name="heart" size={28} color={colors.palette.ivory}/>
-                    <Text>Amour</Text>
+                    <Image source={love} style={styles.icon} />
+                    <View style={styles.direction}>
+                        <Text style={styles.domainText}>Amour</Text>
+                        <Image source={rightArrow} style={styles.iconImage}></Image>
+                    </View>
                 </Pressable>
                 <Pressable style={styles.domainCard} onPress={workChoice}>
-                    <Icon name="building" size={28} color={colors.palette.ivory}/>
-                    <Text>Travail</Text>
+                        <Image source={work} style={styles.icon} />
+                    <View style={styles.direction}>
+                        <Text style={styles.domainText}>Travail</Text>
+                        <Image source={rightArrow} style={styles.iconImage}></Image>
+                    </View>
                 </Pressable>
                 <Pressable style={styles.domainCard} onPress={moneyChoice}>
-                    <Icon name="euro" size={28} color={colors.palette.ivory}/>
-                    <Text>Argent</Text>
+                        <Image source={money} style={styles.icon} />
+                    <View style={styles.direction}>
+                        <Text style={styles.domainText}>Argent</Text>
+                        <Image source={rightArrow} style={styles.iconImage}></Image>
+                        </View>
                 </Pressable>
                 <Pressable style={styles.domainCard} onPress={generalChoice}>
-                    <Icon name="question" size={28} color={colors.palette.ivory}/>
-                    <Text>Général   </Text>
+                        <Image source={general} style={styles.icon} />
+                    <View style={styles.direction}>
+                        <Text style={styles.domainText}>Général</Text>
+                        <Image source={rightArrow} style={styles.iconImage}></Image>
+                        </View>
                 </Pressable>
             </View>
         </LinearGradient >
@@ -93,13 +113,13 @@ const styles = StyleSheet.create({
         flex: 1,
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
-        backgroundColor: colors.palette.outterSpace,
+
         alignItems: 'center',
         justifyContent: 'center',
     },
     icon: {
-        marginRight: 20,
-        color: colors.palette.lightgold,
+        width: '50%',
+        height: '80%',
     },
     input: {
         fontFamily: 'mulishRegular',
@@ -108,28 +128,44 @@ const styles = StyleSheet.create({
     },
     // Domain Container
     domainsContainer: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignContent: 'center',
-        width: SCREEN_WIDTH -20,
-        height: SCREEN_HEIGHT /2,
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
     },
     domainCard: {
-        width: "40%",
-        height: "40%",
+        width: "90%",
+        height: "16%",
         margin: 10,
-        justifyContent: 'space-around',
-        alignItems: 'center',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
         borderWidth: 1,
         borderColor: colors.palette.ivory,
         borderRadius: 10,
         backgroundColor: colors.palette.purple100,
     },
+    domainText: {
+        fontFamily: "mulishBold",
+        fontSize: 26,
+        color: colors.palette.pink500,
+    },
+    iconImage: {
+        width: 30,
+        height: 30,
+        alignSelf: "flex-end",
+        marginLeft: 20,
+    },
     contentTitle: {
-        fontFamily: "mulishRegular",
-        fontSize: 18,
+        fontFamily: "mulishBold",
+        fontSize: 22,
         color: colors.palette.ivory,
         marginBottom: 20
-      }
+    },
+    direction: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingTop: 55
+    },
 })
