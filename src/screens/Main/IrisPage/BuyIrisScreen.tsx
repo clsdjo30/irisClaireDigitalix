af0f6f3b9150c020945076495c17a31f6e9a797d
clsdjo30 <lesergent.cedric@gmail.com>
2023-06-14 18:47:21 +0200

    add style on page

--- src/screens/Main/IrisPage/BuyIrisScreen.tsx
+++ src/screens/Main/IrisPage/BuyIrisScreen.tsx
@@ -9,8 +9,8 @@ import {
     Dimensions
 } from 'react-native';
 import { colors } from '../../../theme';
-import { useCrossQuestionStore } from '../../../utils/hooks/useCrossQuestionStore';
 import { StackScreenProps } from '@react-navigation/stack';
+import { LinearGradient } from 'expo-linear-gradient';
 
 // import icons
 const love = require('../../../../assets/icons/domainIcon/heart_icon.png');
@@ -31,51 +31,131 @@ const SCREEN_HEIGHT = SCREEN_WIDTH * 1.5;
 
 
 const BuyIrisScreen: React.FC<StackScreenProps<any>> = ({ navigation }) => {
-    
+
 
 
     return (
         <View style={styles.container}>
 
             <View style={styles.header} />
-            <View style={styles.domainsContainer}>
-                <View>
+            <View style={styles.titleCredit}>
+                <View style={styles.explainCredit}>
                     <Text style={styles.contentTitle}>Credits</Text>
-                    <Text style={styles.contentTitle}>1 Crédit = 1 Question Oui/Non</Text>
-                    <Text style={styles.contentTitle}>3 Crédits = 1 Tirage Complet</Text>
+                    <Text style={styles.textCredit}>1 Crédit = 1 Question Oui/Non</Text>
+                    <Text style={styles.textCredit}>3 Crédits = 1 Tirage Complet</Text>
                 </View>
-                <Pressable style={styles.domainCard} >
-                    <View style={styles.iconBlock}>
-                        <Image source={love} style={styles.icon} />
-                    </View>
-                    <View style={styles.domainTextBlockBlock}>
-                        <Text style={styles.domainText}>Amour</Text>
-                    </View>
-                </Pressable>
-                <Pressable style={styles.domainCard} >
-                    <View style={styles.iconBlock}>
-                        <Image source={work} style={styles.icon} />
-                    </View>
-                    <View style={styles.domainTextBlockBlock}>
-                        <Text style={styles.domainText}>Travail</Text>
-                    </View>
-                </Pressable>
-                <Pressable style={styles.domainCard} >
-                    <View style={styles.iconBlock}>
-                        <Image source={money} style={styles.icon} />
-                    </View>
-                    <View style={styles.domainTextBlockBlock}>
-                        <Text style={styles.domainText}>Argent</Text>
-                    </View>
-                </Pressable>
-                <Pressable style={styles.domainCard}>
-                    <View style={styles.iconBlock}>
-                        <Image source={general} style={styles.icon} />
-                    </View>
-                    <View style={styles.domainTextBlockBlock}>
-                        <Text style={styles.domainText}>Général</Text>
-                    </View>
-                </Pressable>
+            </View>
+            <View style={styles.domainsContainer}>
+                <LinearGradient
+                    colors={[colors.palette.violetClair, colors.palette.violetBg]}
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    locations={[0.1, 0.9]}
+                    style={styles.domainCard}
+                >
+                    <Pressable
+                        style={styles.innerContainer}
+                    >
+                        <View style={styles.priceBlock}>
+                            <Text style={styles.priceText}>1</Text>
+                            <Text style={styles.priceSubText}>Crédit</Text>
+                        </View>
+                        <View style={styles.domainTextBlock}>
+                            <Text style={styles.price}>  1.99€</Text>
+                        </View>
+                    </Pressable>
+                </LinearGradient>
+
+                <LinearGradient
+                    colors={[colors.palette.violetClair, colors.palette.violetBg]}
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    locations={[0.1, 0.9]}
+                    style={styles.domainCard}
+                >
+                    <Pressable
+                        style={styles.innerContainer}
+                    >
+                        <View style={styles.priceBlock}>
+                            <Text style={styles.priceText}>3</Text>
+                            <Text style={styles.priceSubText}>Crédits</Text>
+                        </View>
+                        <View style={styles.domainTextBlock}>
+                            <View style={styles.promoBlock}>
+                                <Text style={styles.pricePromo}>6.00€</Text>
+                                <View style={styles.bestOffer}>
+                                    <Text style={styles.bestOfferText}>-13%</Text>
+                                </View>
+                            </View>
+                            <Text style={styles.price}>  4.99€</Text>
+                        </View>
+                    </Pressable>
+                </LinearGradient>
+
+                <LinearGradient
+                    colors={[colors.palette.orange, colors.palette.violetBg]}
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    locations={[0.1, 0.9]}
+                    style={styles.domainCard}
+                >
+                    <Pressable
+                        style={styles.innerContainer}
+                    >
+                        <View style={styles.priceBlock}>
+                            <Text style={styles.priceText}>10</Text>
+                            <Text style={styles.priceSubText}>Crédits</Text>
+                        </View>
+                        <View style={styles.bestDeal}>
+                            <Text style={styles.bestDealText}>Best Deal</Text>
+
+                        </View>
+                        <View style={styles.domainTextBlock}>
+                            <Text style={styles.price}>15.99€</Text>
+                        </View>
+                    </Pressable>
+                </LinearGradient>
+
+                <LinearGradient
+                    colors={[colors.palette.violetClair, colors.palette.violetBg]}
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    locations={[0.1, 0.9]}
+                    style={styles.domainCard}
+                >
+                    <Pressable
+                        style={styles.innerContainer}
+                    >
+                        <View style={styles.priceBlock}>
+                            <Text style={styles.priceText}>20</Text>
+                            <Text style={styles.priceSubText}>Crédit</Text>
+                        </View>
+                        <View style={styles.domainTextBlock}>
+                            <Text style={styles.price}>28.99€</Text>
+                        </View>
+                    </Pressable>
+                </LinearGradient>
+
+                <LinearGradient
+                    colors={[colors.palette.violetClair, colors.palette.violetBg]}
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    locations={[0.1, 0.9]}
+                    style={styles.domainCard}
+                >
+                    <Pressable
+                        style={styles.innerContainer}
+                    >
+                        <View style={styles.priceBlock}>
+                            <Text style={styles.priceText}>30</Text>
+                            <Text style={styles.priceSubText}>Crédit</Text>
+                        </View>
+                        <View style={styles.domainTextBlock}>
+                            <Text style={styles.price}>39.99€</Text>
+                        </View>
+                    </Pressable>
+                </LinearGradient>
+
             </View>
         </View>
     )
@@ -101,48 +181,132 @@ const styles = StyleSheet.create({
         borderBottomRightRadius: SCREEN_WIDTH * 0.1,
         backgroundColor: colors.palette.violet
     },
+    // Explain Credit
+    titleCredit: {
+        width: "100%",
+        height: "30%",
+        flexDirection: "column",
+        justifyContent: 'flex-start',
+        alignItems: "center",
+        marginTop: 100,
+    },
+    explainCredit: {
+        width: "90%",
+        height: "30%",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    contentTitle: {
+        fontFamily: "oswaldBold",
+        fontSize: 34,
+        color: colors.palette.ivory,
+        marginBottom: 10,
+    },
+    textCredit: {
+        fontFamily: "mulishMedium",
+        fontSize: 22,
+        color: colors.palette.ivory,
+        paddingVertical: 5,
+    },
     // Domain Container
     domainsContainer: {
-        position: 'absolute', //Here is the trick
-        top: -50,
+        position: 'relative',
+        top: -40,
         width: "100%",
-        height: "100%",
+        height: "80%",
+        flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center",
     },
     domainCard: {
-        width: "80%",
-        height: "10%",
-        margin: 10,
-        flexDirection: "row",
+        width: "90%",
+        height: "15%",
         alignItems: "center",
         borderWidth: 1,
+        marginBottom: 20,
         borderColor: colors.palette.ivory,
         borderRadius: 10,
         backgroundColor: colors.palette.violetClair,
+    },
+    innerContainer: {
+        borderRadius: 15,
+        flexDirection: "row",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "flex-start"
     },
     icon: {
         width: SCREEN_WIDTH * 0.17,
         height: SCREEN_WIDTH * 0.17,
     },
-    iconBlock: {
-        width: '20%',
-        marginLeft: 20,
+    priceBlock: {
+        flexDirection: "column",
+        alignItems: "center",
+        width: '50%',
     },
-    domainTextBlockBlock: {
+    domainTextBlock: {
         width: '80%',
-        marginLeft: 50,
+
+        marginLeft: 40,
     },
-    domainText: {
+    price: {
+        fontFamily: "mulishExtraBold",
+        fontSize: 34,
+        color: colors.palette.violet,
+        marginTop: 20
+    },
+    priceText: {
         fontFamily: "mulishBold",
         fontSize: 20,
         color: colors.palette.violet,
+    },
+    priceSubText: {
+        fontFamily: "mulishBold",
+        fontSize: 20,
+        color: colors.palette.violet,
+    },
+    // Promo Block
+    promoBlock: {
+        position: 'absolute',
+        width: 90,
+        top: 0,
+        left:   20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    bestOffer: {
+        width: 50,
+        height: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.palette.orange,
 
     },
-    contentTitle: {
-        fontFamily: "mulishBold",
-        fontSize: 22,
-        color: colors.palette.ivory,
-        marginBottom: 50
+    pricePromo: {
+        fontFamily: 'mulishLight',
+        fontSize: 10,
+        textDecorationLine: 'line-through'
     },
+    bestOfferText: {
+        fontFamily: 'mulishMedium',
+        fontSize: 10,
+        color: colors.palette.violetBg
+    },
+    bestDeal: {
+        position: 'absolute',
+        top: -12,
+        left: 120,
+        backgroundColor: colors.palette.golden,
+        paddingHorizontal: 20,
+        paddingVertical: 5,
+        borderRadius: 10,
+    },
+    bestDealText: {
+        fontFamily: 'mulishMedium',
+        fontSize: 14,
+        color: colors.palette.violetBg
+    },
+
+
 })
