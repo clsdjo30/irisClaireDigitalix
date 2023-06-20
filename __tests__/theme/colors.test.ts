17672650d1ac41cfc6840ed9b2312298c37f0705
clsdjo30 <lesergent.cedric@gmail.com>
2023-06-20 09:54:35 +0200

    test colors.ts

---
+++ __tests__/theme/colors.test.ts
@@ -0,0 +1,55 @@
+import { colors } from '../../src/theme/color';
+
+describe('Color Palette', () => {
+  test('Should have defined colors', () => {
+    expect(colors).toBeDefined();
+  });
+
+  test('Should have defined text color', () => {
+    expect(colors.text).toBeDefined();
+  });
+
+  test('Should have defined background color', () => {
+    expect(colors.background).toBeDefined();
+  });
+
+  test('Should have defined border color', () => {
+    expect(colors.border).toBeDefined();
+  });
+
+  test('Should have defined tint color', () => {
+    expect(colors.tint).toBeDefined();
+  });
+
+  test('Should have defined separator color', () => {
+    expect(colors.separator).toBeDefined();
+  });
+
+  test('Should have defined error color', () => {
+    expect(colors.error).toBeDefined();
+  });
+
+  test('Should have defined error background color', () => {
+    expect(colors.errorBackground).toBeDefined();
+  });
+
+  test('Should have defined gold color', () => {
+    expect(colors.gold).toBeDefined();
+  });
+
+  test('Should have defined accent color', () => {
+    expect(colors.accent).toBeDefined();
+  });
+
+  test('Should have defined dark gold color', () => {
+    expect(colors.darkgold).toBeDefined();
+  });
+
+  test('Should have defined dark yellow color', () => {
+    expect(colors.darkYellow).toBeDefined();
+  });
+
+  test('Should have defined blue color', () => {
+    expect(colors.blue).toBeDefined();
+  });
+});
